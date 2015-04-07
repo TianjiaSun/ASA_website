e86188d2391d2e3f5496ab06b712359053de4ecf
TianjiaSun <thomas.sun@outlook.com>
2015-04-06 18:59:54 -0700

    remove developer login at api page

--- js/display.js
+++ js/display.js
@@ -3,12 +3,16 @@ $(document).ready(function() {
   $("#start_page").hide();
   $("#what_page").hide();
   $("#store_page").hide();
+    $("#top_bar_id").show();
+    $("#sign_header_id").show();
 
   $("#index_bn").click(function() {
     $("#index_page").show();
     $("#start_page").hide();
     $("#what_page").hide();
     $("#store_page").hide();
+    $("#top_bar_id").show();
+    $("#sign_header_id").show();
   });
 
   $("#what_bn").click(function() {
@@ -16,6 +20,8 @@ $(document).ready(function() {
     $("#start_page").hide();
     $("#what_page").show();
     $("#store_page").hide();
+    $("#top_bar_id").show();
+    $("#sign_header_id").show();
   });
 
   $("#start_bn").click(function() {
@@ -23,6 +29,8 @@ $(document).ready(function() {
     $("#start_page").show();
     $("#what_page").hide();
     $("#store_page").hide();
+    $("#top_bar_id").hide();
+    $("#sign_header_id").hide();
   });
 
   $("#store_bn").click(function() {
@@ -30,6 +38,8 @@ $(document).ready(function() {
     $("#start_page").hide();
     $("#what_page").hide();
     $("#store_page").show();
+    $("#top_bar_id").show();
+    $("#sign_header_id").show();
   });
   
 })
